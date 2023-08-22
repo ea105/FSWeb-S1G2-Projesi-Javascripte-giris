a1dcdce09e7a900fe23f0b5118dc7f4b8d4c7a51
Emre Akdeniz <eakdeniz105@gmail.com>
2023-08-22 20:49:03 +0300

    s1g2

--- index.js
+++ index.js
@@ -52,9 +52,12 @@ Aşağıdakileri yaparak carpma isimli fonksiyonu tamamla:
    3. console.log(carpma(7,4)) ile yazdığın fonsiyonu test edin. Console'da sonucu 28 olarak görmelisin.
 */
 
-function carpma(/*buraya kodunu yazabilirsin*/) {
-  /*buraya kodunu yazabilirsin*/
+function carpma(num1,num2) {
+  return result = num1*num2
 }
+carpma(7,4);
+console.log(carpma(7,4));
+
 
 /* Görev 2 : Köpeğin Yaşı */
 
@@ -65,9 +68,10 @@ Aşağıdakileri yap:
  3. Hesaplanan köpeğin yaşını dönün.
  */
 
-function kopeginYasi(/*buraya kodunu yazabilirsin*/) {
-  /*buraya kodunu yazabilirsin*/
+ function kopeginYasi(yil) {
+  return yil*7
 }
+console.log("köpek",kopeginYasi(3));
 
 /* Görev 3 */
 /*
